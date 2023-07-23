104e4cef967f1ea76def46451e12df3c92a195e8
wangzexiang8 <64294708+wangzexiang8@users.noreply.github.com>
2023-07-23 11:18:08 +0000

    Java

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("6");
+        int a=666;
+        System.out.println(a);
+        System.out.println("---------------------------------");
+        add(25, 1);
+    }
+    public static void add(int num1,int num2){
+            System.out.println(num1+num2);
+    }
+}
