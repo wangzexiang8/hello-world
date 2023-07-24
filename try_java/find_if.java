4165d6ce352aa96a397e08860fdcf9e56005becc
wangzexiang8 <64294708+wangzexiang8@users.noreply.github.com>
2023-07-24 14:33:38 +0000

    ok

--- try_java/find_if.java
+++ try_java/find_if.java
@@ -1,5 +1,24 @@
 import java.util.Scanner;
 
 public class find_if{
-    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("string->");
+        String s = input.next();
+        System.out.print("num->");
+        int num=input.nextInt();
+        input.close();
+        if (num==1) {
+            System.out.println("num=1 is running");
+        }
+        else {
+            System.out.println("num=1 is not running"); 
+        }
+
+        if (s.equals("we")) {
+            System.out.println("s=we is running");
+        } else {
+            System.out.println("s=we is not running");
+        }
+    }
 }
